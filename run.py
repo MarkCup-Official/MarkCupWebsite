f162da6f4273922a823c1d08470055472094abac
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 15:44:37 +0800

    压缩和混淆绳子

--- run.py
+++ run.py
@@ -2,8 +2,6 @@
 文件名: run.py
 作者: MarkCup
 版本: 0.1
-创建日期: 2023-12-25
-最后修改日期: 2023-12-25
 描述: 主程序入点,开启flask后端服务
 """
 
@@ -14,4 +12,4 @@ app = create_app()
 if __name__ == '__main__':
     
     #运行实例
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0",port=80)
